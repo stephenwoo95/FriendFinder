aca1edd97b129addbe9c19cd9079722cb59f75ca
Stephen Woo <stephenwoo95@gmail.com>
2017-08-25 14:31:14 -0700

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,35 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-
-var tableArray = [
+var friendsArr = [{
+    name: "Ahmed",
+    photo: "https://pbs.twimg.com/profile_images/779129231028682752/ijbnCAIa.jpg",
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      5,
+      4,
+      3,
+      2,
+      1
+    ]
+  },
   {
-    customerName: "Ahmed",
-    customerEmail: "ahmed@example.com",
-    customerID: "afhaque89",
-    phoneNumber: "000-000-0000"
+    name: "Bahmed",
+    photo: "http://pcwww.liv.ac.uk/~alabdin/myphoto.jpg",
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      2,
+      2,
+      4,
+      3,
+      2,
+      1
+    ]
   }
 ];
 
-// Note how we export the array. This makes it accessible to other files using require.
-module.exports = tableArray;
+module.exports = friendsArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,62 +1,35 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
+var friendsData = require("../data/friends.js");
 
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
-
-
-// ===============================================================================
 // ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
   // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
-  app.get("/api/tables", function(req, res) {
-    res.json(tableData);
-  });
-
-  app.get("/api/waitlist", function(req, res) {
-    res.json(waitListData);
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
   });
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
 
-  app.post("/api/tables", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
+  app.post("/api/friends", function(req, res) {
+    var bestFit=0;
+    var bestIndex;
+    for(i=0;i<friendsData.length;i++){
+      var fit = 0;
+      for(j=0;j<friendsData[i].scores.length;j++){
+        fit += (Math.abs(friendsData[i].scores[j] - req.body.scores[j]));
+      }
+      if(i==0){
+        bestFit = fit;
+        bestIndex = 0;
+      }
+      else if(fit < bestFit){
+          bestFit = fit;
+          bestIndex = i;
+      }
     }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    friendsData.push(req.body);
+    res.send({name: friendsData[bestIndex].name, photo: friendsData[bestIndex].photo});
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
-
-    console.log(tableData);
-  });
 };
